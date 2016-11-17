2ff508232ed8a29f79b5dbb5aaaa82e3f39b6875
Alexei Labrada <alexei.labrada@cneuro.edu.cu>
2016-11-17 13:10:11 -0500

    Create IObservableResult and its base classes.

---
+++ Neuronic.CollectionModel/IObservableResult.cs
@@ -0,0 +1,30 @@
+using System;
+using System.ComponentModel;
+
+namespace Neuronic.CollectionModel
+{
+    /// <summary>
+    /// Represents an operation's result that can change over time.
+    /// </summary>
+    /// <typeparam name="T">The type of the operation's result</typeparam>
+    /// <seealso cref="System.ComponentModel.INotifyPropertyChanged" />
+    /// <seealso cref="System.ComponentModel.INotifyPropertyChanging" />
+    public interface IObservableResult<out T> : INotifyPropertyChanged, INotifyPropertyChanging
+    {
+        /// <summary>
+        /// Gets the current value.
+        /// </summary>
+        /// <value>
+        /// The current result of the operation.
+        /// </value>
+        T CurrentValue { get; }
+        /// <summary>
+        /// Occurs when the value of <see cref="CurrentValue"/> is changing.
+        /// </summary>
+        event EventHandler CurrentValueChanging;
+        /// <summary>
+        /// Occurs when the value of <see cref="CurrentValue"/> has changed.
+        /// </summary>
+        event EventHandler CurrentValueChanged;
+    }
+}
---
+++ Neuronic.CollectionModel/SimpleQueryObservableResult.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Specialized;
+
+namespace Neuronic.CollectionModel
+{
+    /// <summary>
+    /// A simple observable query that re-evaluated the query every time the source changes.
+    /// </summary>
+    /// <typeparam name="TSource">The type of the source collection.</typeparam>
+    /// <typeparam name="TResult">The type of the result.</typeparam>
+    /// <seealso cref="Neuronic.CollectionModel.QueryObservableResult{TSource, TResult}" />
+    /// <remarks>
+    /// <para>
+    /// This class is useful for O(1) queries (i.e. FirstOrDefault) and queries that
+    /// would require to always be re-evaluated when the source collection changes. 
+    /// </para>
+    /// </remarks>
+    public class SimpleQueryObservableResult<TSource, TResult> : QueryObservableResult<TSource, TResult>
+    {
+        private readonly Func<IReadOnlyObservableCollection<TSource>, TResult> _query;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SimpleQueryObservableResult{TSource, TResult}"/> class.
+        /// </summary>
+        /// <param name="source">The source.</param>
+        /// <param name="query">The query.</param>
+        public SimpleQueryObservableResult(IReadOnlyObservableCollection<TSource> source, Func<IReadOnlyObservableCollection<TSource>, TResult> query) : base(source)
+        {
+            _query = query;
+        }
+
+        /// <summary>
+        /// Called when the source collection changes.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="T:System.Collections.Specialized.NotifyCollectionChangedEventArgs" /> instance containing the event data.</param>
+        protected override void OnSourceChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            CurrentValue = _query((IReadOnlyObservableCollection<TSource>) sender);
+        }
+    }
+}
