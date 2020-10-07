b04293f94f31a9abdb868ae5323aaebad4f6a56e
Alexei Labrada <alexei.labrada@cneuro.edu.cu>
2020-10-07 10:58:35 -0500

    Implement prefix ROOL.

---
+++ Neuronic.CollectionModel.Testing/PrefixReadOnlyObservableListTest.cs
@@ -0,0 +1,347 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neuronic.CollectionModel.Collections;
+
+namespace Neuronic.CollectionModel.Testing
+{
+    [TestClass]
+    public class PrefixReadOnlyObservableListTest
+    {
+        [TestMethod]
+        public void IteratorTest()
+        {
+            var observableCollection = new ObservableCollection<int>(new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+            var prool = new PrefixReadOnlyObservableList<int>(observableCollection, i => i <= 5);
+
+            Assert.IsTrue(observableCollection.TakeWhile(i => i <= 5).SequenceEqual(prool));
+        }
+
+        /// <summary>
+        /// Testing Add method several times and Inset method.
+        /// </summary>
+        [TestMethod]
+        public void AddItemsTest()
+        {
+            var observableCollection = new ObservableCollection<int>(new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+            var rool = new ReadOnlyObservableList<int>(observableCollection);
+
+            var prool = new PrefixReadOnlyObservableList<int>(rool, i => i <= 5);
+            var copy = prool.ListSelect(x => x);
+
+            observableCollection.Add(2);
+            Assert.IsTrue(observableCollection.TakeWhile(i => i <= 5).SequenceEqual(prool));
+            Assert.IsTrue(observableCollection.TakeWhile(i => i <= 5).SequenceEqual(copy));
+
+            observableCollection.Insert(3, 11);
+            Assert.IsTrue(observableCollection.TakeWhile(i => i <= 5).SequenceEqual(prool));
+            Assert.IsTrue(observableCollection.TakeWhile(i => i <= 5).SequenceEqual(copy));
+
+            observableCollection.Insert(0, 0);
+            Assert.IsTrue(observableCollection.TakeWhile(i => i <= 5).SequenceEqual(prool));
+            Assert.IsTrue(observableCollection.TakeWhile(i => i <= 5).SequenceEqual(copy));
+        }
+
+        /// <summary>
+        /// Testing Remove and RemoveAt method.
+        /// </summary>
+        [TestMethod]
+        public void DeleteItemsTest()
+        {
+            var observableCollection = new ObservableCollection<int>(new List<int>() { 1, 2, 3, 4, 5, 6, 5, 4, 9, 10 });
+            var rool = new ReadOnlyObservableList<int>(observableCollection);
+
+            var prool = new PrefixReadOnlyObservableList<int>(rool, i => i <= 5);
+            var copy = prool.ListSelect(x => x);
+
+            observableCollection.RemoveAt(9);
+            Assert.IsTrue(observableCollection.TakeWhile(i => i <= 5).SequenceEqual(prool));
+            Assert.IsTrue(observableCollection.TakeWhile(i => i <= 5).SequenceEqual(copy));
+
+            observableCollection.Remove(6);
+            Assert.IsTrue(observableCollection.TakeWhile(i => i <= 5).SequenceEqual(prool));
+            Assert.IsTrue(observableCollection.TakeWhile(i => i <= 5).SequenceEqual(copy));
+
+            observableCollection.RemoveAt(1);
+            Assert.IsTrue(observableCollection.TakeWhile(i => i <= 5).SequenceEqual(prool));
+            Assert.IsTrue(observableCollection.TakeWhile(i => i <= 5).SequenceEqual(copy));
+        }
+
+        /// <summary>
+        /// Testing Move method.
+        /// </summary>
+        [TestMethod]
+        public void MoveItemsTest()
+        {
+            var observableCollection = new ObservableCollection<int>(new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+            var rool = new ReadOnlyObservableList<int>(observableCollection);
+
+            var frooc = new FilteredReadOnlyObservableList<int>(rool, i => i % 2 == 0);
+            Assert.AreEqual(frooc.Count, 5);
+
+            //[2 4 6 8 10]
+            observableCollection.Move(1, 7);
+            //[4 6 8 2 10]
+            observableCollection.Move(3, 5);
+            //[4 6 8 2 10]
+            observableCollection.Move(3, 2);
+            //[6 4 8 2 10]
+
+            int[] a = new[] { 6, 4, 8, 2, 10 };
+
+            var iterator = frooc.GetEnumerator();
+            int j = 0;
+            while (iterator.MoveNext())
+            {
+                Assert.AreEqual(iterator.Current, a[j]);
+                j++;
+            }
+            Assert.AreEqual(frooc.Count, 5);
+        }
+
+        /// <summary>
+        /// Testing several operations: Remove, Move, Add, Inser and RemoveAt method and indexer of class.
+        /// </summary>
+        [TestMethod]
+        public void OperationsTest()
+        {
+            var observableCollection = new ObservableCollection<int>(new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 });
+            var rool = new ReadOnlyObservableList<int>(observableCollection);
+
+            var frooc = new FilteredReadOnlyObservableList<int>(rool, i => i % 2 == 0);
+            Assert.AreEqual(frooc.Count, 10);
+
+            //[2 4 6 8 10 12 14 16 18 20]
+            observableCollection.Remove(4);
+            //[2 6 8 10 12 14 16 18 20]
+            observableCollection.Move(6, 13);
+            //[2 6 10 12 14 8 16 18 20]
+            observableCollection.Add(22);
+            //[2 6 10 12 14 8 16 18 20 22]
+            observableCollection.Insert(3, 4);
+            //[2 4 6 10 12 14 8 16 18 20 22]
+            observableCollection.RemoveAt(20);
+            //[2 4 6 10 12 14 8 16 18 20]
+            observableCollection[0] = 24;
+            //[24 2 4 6 10 12 14 8 16 18 20]
+
+            int[] a = new[] { 24, 2, 4, 6, 10, 12, 14, 8, 16, 18, 20 };
+
+            var iterator = frooc.GetEnumerator();
+            int j = 0;
+            while (iterator.MoveNext())
+            {
+                Assert.AreEqual(iterator.Current, a[j]);
+                j++;
+            }
+            Assert.AreEqual(frooc.Count, a.Length);
+        }
+
+        /// <summary>
+        /// Testing PropertyChanged event.
+        /// </summary>
+        [TestMethod]
+        public void NotifyFilterTest()
+        {
+            var collection = new List<Notify>() { new Notify(), new Notify(), new Notify(), new Notify(), new Notify(), new Notify(), new Notify() };
+            var observableCollection = new ObservableCollection<Notify>(collection);
+            observableCollection[0].Prop = 1;
+            observableCollection[1].Prop = 2;
+            observableCollection[2].Prop = 3;
+            observableCollection[3].Prop = 4;
+            observableCollection[4].Prop = 5;
+            observableCollection[5].Prop = 6;
+            observableCollection[6].Prop = 7;
+            var rool = new ReadOnlyObservableList<Notify>(observableCollection);
+
+            FilteredReadOnlyObservableList<Notify> frooc = new FilteredReadOnlyObservableList<Notify>(rool, notify => notify.Prop % 2 == 0, nameof(Notify.Prop));
+            Assert.AreEqual(frooc.Count, 3);
+
+            int i = 0;
+            foreach (var result in frooc.Zip(collection.Where(c => c.Prop % 2 == 0), (actual, expected) => new { actual, expected }))
+            {
+                Assert.AreEqual(result.expected, result.actual);
+                i++;
+            }
+            Assert.AreEqual(i, 3);
+
+            observableCollection[1].Prop = 5;
+
+            Assert.AreEqual(frooc.Count, 2);
+            i = 0;
+            foreach (var result in frooc.Zip(collection.Where(c => c.Prop % 2 == 0), (actual, expected) => new { actual, expected }))
+            {
+                Assert.AreEqual(result.expected, result.actual);
+                i++;
+            }
+            Assert.AreEqual(i, 2);
+
+            observableCollection[4].Prop = 8;
+
+            Assert.AreEqual(frooc.Count, 3);
+            i = 0;
+            foreach (var result in frooc.Zip(collection.Where(c => c.Prop % 2 == 0), (actual, expected) => new { actual, expected }))
+            {
+                Assert.AreEqual(result.expected, result.actual);
+                i++;
+            }
+            Assert.AreEqual(i, 3);
+        }
+
+        /// <summary>
+        /// Testing PropertyChanged event.
+        /// </summary>
+        [TestMethod]
+        public void NotifyFilterAutoPropertyTest()
+        {
+            var collection = new List<Notify>() { new Notify(), new Notify(), new Notify(), new Notify(), new Notify(), new Notify(), new Notify() };
+            var observableCollection = new ObservableCollection<Notify>(collection);
+            observableCollection[0].Prop = 1;
+            observableCollection[1].Prop = 2;
+            observableCollection[2].Prop = 3;
+            observableCollection[3].Prop = 4;
+            observableCollection[4].Prop = 5;
+            observableCollection[5].Prop = 6;
+            observableCollection[6].Prop = 7;
+            var rool = new ReadOnlyObservableList<Notify>(observableCollection);
+
+            FilteredReadOnlyObservableList<Notify> frooc = new FilteredReadOnlyObservableList<Notify>(rool, notify => notify.Observe(x => x.Prop % 2 == 0));
+            Assert.AreEqual(frooc.Count, 3);
+
+            int i = 0;
+            foreach (var result in frooc.Zip(collection.Where(c => c.Prop % 2 == 0), (actual, expected) => new { actual, expected }))
+            {
+                Assert.AreEqual(result.expected, result.actual);
+                i++;
+            }
+            Assert.AreEqual(i, 3);
+
+            observableCollection[1].Prop = 5;
+
+            Assert.AreEqual(frooc.Count, 2);
+            i = 0;
+            foreach (var result in frooc.Zip(collection.Where(c => c.Prop % 2 == 0), (actual, expected) => new { actual, expected }))
+            {
+                Assert.AreEqual(result.expected, result.actual);
+                i++;
+            }
+            Assert.AreEqual(i, 2);
+
+            observableCollection[4].Prop = 8;
+
+            Assert.AreEqual(frooc.Count, 3);
+            i = 0;
+            foreach (var result in frooc.Zip(collection.Where(c => c.Prop % 2 == 0), (actual, expected) => new { actual, expected }))
+            {
+                Assert.AreEqual(result.expected, result.actual);
+                i++;
+            }
+            Assert.AreEqual(i, 3);
+        }
+
+        /// <summary>
+        /// Testing PropertyChanged event.
+        /// </summary>
+        [TestMethod]
+        public void NotifyFilterObserveAllTest()
+        {
+            var collection = new List<Notify>() { new Notify(), new Notify(), new Notify(), new Notify(), new Notify(), new Notify(), new Notify() };
+            var observableCollection = new ObservableCollection<Notify>(collection);
+            observableCollection[0].Prop = 1;
+            observableCollection[1].Prop = 2;
+            observableCollection[2].Prop = 3;
+            observableCollection[3].Prop = 4;
+            observableCollection[4].Prop = 5;
+            observableCollection[5].Prop = 6;
+            observableCollection[6].Prop = 7;
+            var rool = new ReadOnlyObservableList<Notify>(observableCollection);
+
+            FilteredReadOnlyObservableList<Notify> frooc = new FilteredReadOnlyObservableList<Notify>(rool, notify => notify.ObserveAll(x => x.Prop % 2 == 0));
+            Assert.AreEqual(frooc.Count, 3);
+
+            int i = 0;
+            foreach (var result in frooc.Zip(collection.Where(c => c.Prop % 2 == 0), (actual, expected) => new { actual, expected }))
+            {
+                Assert.AreEqual(result.expected, result.actual);
+                i++;
+            }
+            Assert.AreEqual(i, 3);
+
+            observableCollection[1].Prop = 5;
+
+            Assert.AreEqual(frooc.Count, 2);
+            i = 0;
+            foreach (var result in frooc.Zip(collection.Where(c => c.Prop % 2 == 0), (actual, expected) => new { actual, expected }))
+            {
+                Assert.AreEqual(result.expected, result.actual);
+                i++;
+            }
+            Assert.AreEqual(i, 2);
+
+            observableCollection[4].Prop = 8;
+
+            Assert.AreEqual(frooc.Count, 3);
+            i = 0;
+            foreach (var result in frooc.Zip(collection.Where(c => c.Prop % 2 == 0), (actual, expected) => new { actual, expected }))
+            {
+                Assert.AreEqual(result.expected, result.actual);
+                i++;
+            }
+            Assert.AreEqual(i, 3);
+        }
+
+        /// <summary>
+        /// Testing PropertyChanged event.
+        /// </summary>
+        [TestMethod]
+        public void ObservableFilterTest()
+        {
+            var collection = new List<Observable>() { new Observable(), new Observable(), new Observable(), new Observable(), new Observable(), new Observable(), new Observable() };
+            var observableCollection = new ObservableCollection<Observable>(collection);
+            observableCollection[0].Prop = 1;
+            observableCollection[1].Prop = 2;
+            observableCollection[2].Prop = 3;
+            observableCollection[3].Prop = 4;
+            observableCollection[4].Prop = 5;
+            observableCollection[5].Prop = 6;
+            observableCollection[6].Prop = 7;
+            var rool = new ReadOnlyObservableList<Observable>(observableCollection);
+
+            FilteredReadOnlyObservableList<Observable> frooc = new FilteredReadOnlyObservableList<Observable>(rool, notify => notify.Subject.Select(x => x % 2 == 0));
+            Assert.AreEqual(frooc.Count, 3);
+
+            int i = 0;
+            foreach (var result in frooc.Zip(collection.Where(c => c.Prop % 2 == 0), (actual, expected) => new { actual, expected }))
+            {
+                Assert.AreEqual(result.expected, result.actual);
+                i++;
+            }
+            Assert.AreEqual(i, 3);
+
+            observableCollection[1].Prop = 5;
+
+            Assert.AreEqual(frooc.Count, 2);
+            i = 0;
+            foreach (var result in frooc.Zip(collection.Where(c => c.Prop % 2 == 0), (actual, expected) => new { actual, expected }))
+            {
+                Assert.AreEqual(result.expected, result.actual);
+                i++;
+            }
+            Assert.AreEqual(i, 2);
+
+            observableCollection[4].Prop = 8;
+
+            Assert.AreEqual(frooc.Count, 3);
+            i = 0;
+            foreach (var result in frooc.Zip(collection.Where(c => c.Prop % 2 == 0), (actual, expected) => new { actual, expected }))
+            {
+                Assert.AreEqual(result.expected, result.actual);
+                i++;
+            }
+            Assert.AreEqual(i, 3);
+        }
+    }
+}
---
+++ Neuronic.CollectionModel/Collections/PrefixReadOnlyObservableList.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using Neuronic.CollectionModel.Collections.Containers;
+using Neuronic.CollectionModel.Observables;
+using Neuronic.CollectionModel.WeakEventPattern;
+using System.Windows;
+
+namespace Neuronic.CollectionModel.Collections
+{
+    public class PrefixReadOnlyObservableList<T>: IndexedReadOnlyObservableListBase<T, bool>, 
+        IReadOnlyObservableList<T>, IWeakEventListener
+    {
+        private readonly IReadOnlyList<T> _source;
+        private readonly PrefixList _items;
+
+        public PrefixReadOnlyObservableList(IReadOnlyList<T> source, Func<T, IObservable<bool>> selector, 
+            Action<bool> onRemove = null, Action<bool, bool> onChange = null) 
+            : base(null, selector, onRemove, onChange)
+        {
+            _source = source;
+
+            _items = new PrefixList(this);
+            if (_source is INotifyCollectionChanged notifier)
+                CollectionChangedEventManager.AddListener(notifier, this);
+        }
+
+        private PrefixReadOnlyObservableList(IReadOnlyList<T> source, PropertyObservableFactory<T, bool> selector,
+            Action<bool> onRemove = null, Action<bool, bool> onChange = null)
+            : this(source, selector.Observe, onRemove, onChange)
+        {
+        }
+
+        public PrefixReadOnlyObservableList(IReadOnlyList<T> source, Expression<Func<T, bool>> selector,
+            Action<bool> onRemove = null, Action<bool, bool> onChange = null)
+            : this(source, PropertyObservableFactory<T,bool>.FindIn(selector), onRemove, onChange)
+        {
+        }
+
+        public int Count => Items.Count - 1;
+
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public T this[int index] => Items[index].Item;
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return Items.Take(Count).Select(x => x.Item).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        protected override void ItemsOnPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            OnPropertyChanged(e);
+        }
+
+        protected override void ItemsOnCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            var newItems = new List<T>(e.NewItems?.Count ?? 0);
+            if (e.NewItems != null && e.NewItems.Count > 0)
+                newItems.AddRange(e.NewItems.OfType<IndexedItemContainer<T, bool>>().Select(x => x.Item));
+
+            var oldItems = new List<T>(e.OldItems?.Count ?? 0);
+            if (e.OldItems != null && e.OldItems.Count > 0)
+                oldItems.AddRange(e.OldItems.OfType<IndexedItemContainer<T, bool>>().Select(x => x.Item));
+
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, newItems, e.NewStartingIndex));
+                    break;
+
+                case NotifyCollectionChangedAction.Remove:
+                    OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, oldItems, e.OldStartingIndex));
+                    break;
+
+                case NotifyCollectionChangedAction.Replace:
+                    OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Replace, newItems, oldItems, e.NewStartingIndex));
+                    break;
+
+                case NotifyCollectionChangedAction.Move:
+                    OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Move, oldItems, e.NewStartingIndex, e.OldStartingIndex));
+                    break;
+
+                case NotifyCollectionChangedAction.Reset when newItems.Count > 0 && e.NewStartingIndex >= 0:
+                    OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset, newItems, e.NewStartingIndex));
+                    break;
+
+                case NotifyCollectionChangedAction.Reset:
+                    OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+                    break;
+            }
+        }
+
+        protected override void ContainerOnValueChanged(object sender, ValueChangedEventArgs<bool> e)
+        {
+            base.ContainerOnValueChanged(sender, e);
+            var container = (IndexedItemContainer<T, bool>)sender;
+            OnPropertyChanged(new PropertyChangedEventArgs("Item[]"));
+            if (e.NewValue)
+            {
+                OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, container.Item, container.Index));
+                _items.Grow();
+            }
+            else
+            {
+                OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, container.Item, container.Index));
+                _items.ReduceUpTo(container.Index);
+            }
+        }
+
+        bool IWeakEventListener.ReceiveWeakEvent(Type managerType, object sender, EventArgs e)
+        {
+            if (managerType != typeof(CollectionChangedEventManager))
+                return false;
+            if (ReferenceEquals(_source, sender))
+            {
+                _items.UpdateCollection(_source, (NotifyCollectionChangedEventArgs) e, 
+                    o => CreateContainer((T) o), RemoveContainer);
+                _items.Grow();
+            }
+            return true;
+        }
+
+        protected virtual void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
+        {
+            CollectionChanged?.Invoke(this, e);
+        }
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            PropertyChanged?.Invoke(this, e);
+        }
+
+        class PrefixList : IList<IndexedItemContainer<T, bool>>
+        {
+            private readonly PrefixReadOnlyObservableList<T> _owner;
+
+            public PrefixList(PrefixReadOnlyObservableList<T> owner)
+            {
+                _owner = owner ?? throw new ArgumentNullException(nameof(owner));
+                Grow();
+            }
+
+            protected IReadOnlyList<T> Source => _owner._source;
+            protected ObservableCollection<IndexedItemContainer<T, bool>> Containers => _owner.Items;
+
+            public void Grow()
+            {
+                while (Containers.Count < Source.Count && 
+                       (Containers.Count == 0 || Containers[Containers.Count - 1].Value))
+                {
+                    var container = _owner.CreateContainer(Source[Containers.Count]);
+                    Containers.Add(container);
+                }
+            }
+
+            public void ReduceUpTo(int index)
+            {
+                for (int i = Containers.Count - 1; i > index; i--)
+                {
+                    _owner.RemoveContainer(Containers[i]);
+                    Containers.RemoveAt(i);
+                }
+            }
+
+            public void Add(IndexedItemContainer<T, bool> item)
+            {
+                if (Containers.Count == 0 || Containers[Containers.Count - 1].Value)
+                    Containers.Add(item);
+            }
+
+            public void Clear()
+            {
+                Containers.Clear();
+            }
+
+            public bool Contains(IndexedItemContainer<T, bool> item)
+            {
+                return Source.Contains(item.Item);
+            }
+
+            public void CopyTo(IndexedItemContainer<T, bool>[] array, int arrayIndex)
+            {
+                Containers.CopyTo(array, arrayIndex);
+            }
+
+            public bool Remove(IndexedItemContainer<T, bool> item)
+            {
+                Containers.Remove(item);
+                return true;
+            }
+
+            public void RemoveAt(int index)
+            {
+                if (index < Containers.Count)
+                    Containers.RemoveAt(index);
+            }
+
+            public void Insert(int index, IndexedItemContainer<T, bool> item)
+            {
+                if (index > Containers.Count)
+                    return;
+                Containers.Insert(index, item);
+                if (!item.Value)
+                    ReduceUpTo(index);
+            }
+
+            public int Count => Source.Count;
+
+            public bool IsReadOnly => false;
+
+            public int IndexOf(IndexedItemContainer<T, bool> item)
+            {
+                return Source.IndexOf(item.Item);
+            }
+
+            public IndexedItemContainer<T, bool> this[int index]
+            {
+                get => index < Containers.Count ? Containers[index] : _owner.CreateContainer(Source[index]);
+                set
+                {
+                    Containers[index] = value;
+                    if (!value.Value)
+                        ReduceUpTo(index);
+                }
+            }
+
+            public IEnumerator<IndexedItemContainer<T, bool>> GetEnumerator()
+            {
+                return Containers.Concat(Source.Skip(Containers.Count).Select(_owner.CreateContainer)).GetEnumerator();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return ((IEnumerable)Containers).GetEnumerator();
+            }
+        }
+    }
+}
